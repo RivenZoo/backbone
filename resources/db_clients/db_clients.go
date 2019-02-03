7f660db1ee309ff5e63af88473f4e43d32f867f6
zrw <zrw2010@gmail.com>
2019-02-03 23:22:44 +0800

    refactor: refactor db client

--- resources/db_clients/db_clients.go
+++ resources/db_clients/db_clients.go
@@ -7,10 +7,14 @@ import (
 	"github.com/RivenZoo/backbone/resources"
 	"github.com/RivenZoo/dsncfg"
 	"github.com/RivenZoo/sqlagent"
-	"github.com/jmoiron/sqlx/reflectx"
-	"strings"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var sqlagentContainer *namedSqlAgents
+
+func GetBootConfigKey() string {
+	return "res.db_clients"
+}
 
 type sqlConfig struct {
 	ColumnMapTag string `json:"column_map_tag"`
@@ -52,64 +56,6 @@ type dbConfig struct {
 // }
 type DBConfig struct {
 	NamedDBs map[string]dbConfig `json:"dbs"`
-}
-
-type namedSqlAgent struct {
-	name     string
-	sqlagent *sqlagent.SqlAgent
-}
-
-type namedSqlAgents struct {
-	sqlagents []namedSqlAgent
-	config    []dbConfig
-}
-
-func newNamedSqlAgent(cfg *DBConfig) (*namedSqlAgents, error) {
-	ret := &namedSqlAgents{
-		sqlagents: make([]namedSqlAgent, 0, len(cfg.NamedDBs)),
-		config:    make([]dbConfig, 0, len(cfg.NamedDBs)),
-	}
-	for name, dbCfg := range cfg.NamedDBs {
-		sa, err := sqlagent.NewSqlAgent(&dbCfg.DB)
-		if err != nil {
-			return nil, err
-		}
-		ret.sqlagents = append(ret.sqlagents, namedSqlAgent{
-			name:     name,
-			sqlagent: sa,
-		})
-		ret.config = append(ret.config, dbCfg)
-	}
-	return ret, nil
-}
-
-func setSqlConfig(sa *sqlagent.SqlAgent, cfg sqlConfig) {
-	if cfg.ColumnMapTag != "" {
-		sa.SetDBMapper(reflectx.NewMapperFunc(cfg.ColumnMapTag, strings.ToLower))
-	}
-}
-
-func (na *namedSqlAgents) Init() error {
-	for i := range na.config {
-		na.sqlagents[i].sqlagent.SetConnectionConfig(na.config[i].Connection)
-		setSqlConfig(na.sqlagents[i].sqlagent, na.config[i].Sql)
-	}
-	return nil
-}
-
-func (na *namedSqlAgents) Close() error {
-	for i := range sqlagentContainer.sqlagents {
-		if sqlagentContainer.sqlagents[i].sqlagent != nil {
-			sqlagentContainer.sqlagents[i].sqlagent.Close()
-		}
-	}
-	return nil
-}
-
-var sqlagentContainer *namedSqlAgents
-
-func GetBootConfigKey() string {
-	return "res.db_clients"
 }
 
 func GetClient(name string) *sqlagent.SqlAgent {
